0428779703047a35e36d892056f24912e8ce244a
Stopa Arthur <arthur.stopa@fatec.sp.gov.br>
2025-05-23 18:57:26 -0300

    Pasta raiz correta

---
+++ src/main/java/com/stopajogos/dslist/Game.java
@@ -0,0 +1,112 @@
+package com.stopajogos.dslist;
+
+import jakarta.persistence.*;
+import java.util.Objects;
+import jakarta.persistence.Entity;
+
+// Configurando a classe Game para uma tabela relacional no nosso banco de dados. Tratando as estruturas de uma tabela
+@Entity
+@Table(name = "tb_game")
+public class Game {
+    // Criando os atributos
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY /* Uma forma de indicar pro banco para gerar o id*/)
+    private Long id;
+    private String title;
+
+    @Column(name = "game_year" /*Customizando o nome da coluna no SQL, pois year e uma palavra reservada do SQL*/)
+    private Integer year; // Usamos Integer quando necessita-se de um valor nulo quando num BD
+    private String genre;
+    private String platform;
+    private String imgUrl;
+    private String shortDescription;
+    private String longDescription;
+
+    // Construtor
+    public Game(Long id) {
+        this.id = id;
+        this.title = title;
+        this.year = year;
+        this.genre = genre;
+        this.platform = platform;
+        this.imgUrl = imgUrl;
+        this.shortDescription = shortDescription;
+        this.longDescription = longDescription;
+    }
+
+    // Metodos Getters e setters
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Integer getYear() {
+        return year;
+    }
+    public void setYear(Integer year) {
+        this.year = year;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    public String getPlatform() {
+        return platform;
+    }
+    public void setPlatform(String platform) {
+        this.platform = platform;
+    }
+
+    public String getImgUrl() {
+        return imgUrl;
+    }
+    public void setImgUrl(String imgUrl) {
+        this.imgUrl = imgUrl;
+    }
+
+    public String getShortDescription() {
+        return shortDescription;
+    }
+    public void setShortDescription(String shortDescription) {
+        this.shortDescription = shortDescription;
+    }
+
+    public String getLongDescription() {
+        return longDescription;
+    }
+    public void setLongDescription(String longDescription) {
+        this.longDescription = longDescription;
+    }
+
+    // Metodos de comparação dos games, comparação entre objetos
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Game other = (Game) obj;
+        return Objects.equals(id, other.id);
+    }
+}
+
